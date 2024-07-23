7bded427428ef20d3839012e2f3397c620d467f3
jangjeongha <jjha0125@nate.com>
2024-07-23 18:20:34 +0900

    0723

--- my-react/src_Step04_API/App.js
+++ my-react/src_Step04_API/App.js
@@ -1,7 +1,6 @@
 
 import { Component } from 'react';
 import './App.css'
-import axios from 'axios';
 
 //클래스형 component
 class App extends Component{
@@ -17,14 +16,13 @@ class App extends Component{
 
   //전체 글 목록을 받아오는 함수 
   getPosts = ()=>{
-    // axios가 get 방식 "v2/posts" 요청을 대신 해준다
-    // 응답되는 json 문자열을 실제 array or object 로 변경해서 then() 함수 안에 전달한 함수의 매개변수 res 에 넣어준다.
-    // res 는 object 이고 res 의 data 라는 방에 array or object 가 들어있다.
-    axios.get("/v2/posts")
-    .then(res=>{
-      console.log(res)
+    fetch("/v1/posts")
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data)
+      //서버로 부터 응답된 data 로 상태값을 변경한다(UI 는 자동 update)
       this.setState({
-        posts:res.data
+        posts:data
       })
     })
   }
@@ -33,27 +31,26 @@ class App extends Component{
     return (
       <div className="container">
         <h3>새글 작성 폼</h3>
-        <form action="/v2/posts" method="post" onSubmit={(e)=>{
+        <form action="/v1/posts" method="post" onSubmit={(e)=>{
           e.preventDefault();//폼 전송 막기 
           //요청 url 
           const url=e.target.action;
           //전송할 폼 데이터
           const formData=new FormData(e.target);
-          //FormData 에 들어있는 내욜을 이용해서 json 문자열 만들어내기
-          const formObject={}
-          formData.forEach((value,key)=>{
-            formObject[key]=value;
-          });
-          // const jsonString = JSON.stringify(formObject);
-
-          // object 를 json 문자열로 변경할 필요 없이 object 를 두번째 인자로 전달하거나 .
-          // query 문자열을 전달하면 된다(두가지 모두 가능)
-
-          //axios.post(요청 url, object or query 문자열)
-          axios.post(url, formObject)
-          .then(res=>{
+          const queryString=new URLSearchParams(formData).toString();
+          fetch(url, {
+            method:"post",
+            headers:{"Content-Type":"application/x-www-form-urlencoded; charset=utf-8"},
+            body:queryString
+          })
+          .then(res=>res.json())
+          .then(data=>{
+            alert(data.id+ "번 post 로 등록 되었습니다");
             this.getPosts();
           })
+          .catch(error=>{
+            console.log("에러 발생", error)
+          });
         }}>
           <input type="text" name="title" placeholder='제목...'/>
           <input type="text" name="author" placeholder='작성자...'/>
@@ -78,29 +75,36 @@ class App extends Component{
                 <td>{item.author}</td>
                 <td><button onClick={()=>{
                   const title=prompt(item.id+" 번글의 수정할 제목 입력...")
-                  //수정할 정보를 Object 객체에 담는다.
-                  const obj = {
-                    title:title,
-                    author:item.author
-                  }
+                  //수정할 정보를 FormData 객체에 담는다.
+                  const formData = new FormData();
+                  formData.append("title", title);
+                  formData.append("author", item.author);
+                  const queryString=new URLSearchParams(formData).toString();
 
-                  //axios.put(요청 url, object)
-                  axios.put("/v2/posts/"+item.id, obj)
-                  .then(res=>{
-                    this.getPosts();
+                  fetch("/v1/posts/"+item.id, {
+                    method:"put",
+                    headers:{"Content-Type":"application/x-www-form-urlencoded; charset=utf-8"},
+                    body:queryString
                   })
+                  .then(res=>res.json())
+                  .then(data=>{
+                    this.getPosts();
+                  });
 
                 }}>수정</button></td>
                 <td><button onClick={()=>{
-                  axios.delete("/v2/posts/"+item.id)
-                  .then(res=>{
+                  fetch("/v1/posts/"+item.id,{
+                    method:"delete"
+                  })
+                  .then(res=>res.json())
+                  .then(data=>{
+                    //목록을 다시 받아와서 화면 update
+                    //this.getPosts();
+                    //현재 목록에서 삭제된 아이템을 제거한 새로운 배열로 state 변경하기 
                     this.setState({
-                      posts:this.state.posts.filter(it=>res.data !== it.id)
+                      posts:this.state.posts.filter(it=> data.id !== it.id)
                     })
-                  })
-                  .catch(err=>{
-                    console.log(err)
-                  })
+                  })  
                 }}>x</button></td>
               </tr>
             )}
@@ -110,5 +114,6 @@ class App extends Component{
     )
   }
 }
+
 
 export default App;
